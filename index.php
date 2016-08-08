39f25fbcec125d89b6b717b08acf75832ef6c0e2
EpixFr <epix@konectik.fr>
2016-08-08 18:04:31 +0200

    ajout du favicon perso png

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="description" content="Portail Projets Web- Epix &copy; 2016">
 	<meta name="author" content="Epix">
-	<link rel="icon" href="favicon.ico">
+	<link rel="icon" type="image/png" href="favicon.png" />
 
 	<title>PPW - <?php echo $_SERVER['SERVER_NAME']; ?></title>
 
@@ -34,6 +34,7 @@
 			<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 			<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 		<![endif]-->
+	
 </head>
 <body>
 	<nav class="navbar navbar-inverse navbar-fixed-top">
